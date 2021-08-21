990d776d443f17eb0c5468d39b3548966c77c8b4
TakoBigvava <87258382+TakoBigvava@users.noreply.github.com>
2021-08-21 18:59:33 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,77 +1,74 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+	".-": "a",
+	"-...": "b",
+	"-.-.": "c",
+	"-..": "d",
+	".": "e",
+	"..-.": "f",
+	"--.": "g",
+	"....": "h",
+	"..": "i",
+	".---": "j",
+	"-.-": "k",
+	".-..": "l",
+	"--": "m",
+	"-.": "n",
+	"---": "o",
+	".--.": "p",
+	"--.-": "q",
+	".-.": "r",
+	"...": "s",
+	"-": "t",
+	"..-": "u",
+	"...-": "v",
+	".--": "w",
+	"-..-": "x",
+	"-.--": "y",
+	"--..": "z",
+	".----": "1",
+	"..---": "2",
+	"...--": "3",
+	"....-": "4",
+	".....": "5",
+	"-....": "6",
+	"--...": "7",
+	"---..": "8",
+	"----.": "9",
+	"-----": "0",
 };
 
-function decode(expr) {
-    let newArr = [];
-	let morseArr = [];
+	function decode(expr) {
+		// const reg = /^[00]$/g;
+		const reg = /(..........)/g;
+		const reg4 = /\*\*\*\*\*\*\*\*\*\*/g;
+		const reg1 = /10/g;
+		const reg2 = /11/g;
+		 const reg3 = /00/g;
+		let letters = []
+		let decoded = []
+	  
+		// array of words
+	  const words = expr.replace(reg4, " ").split(' ');
+	  
+	   //array of digits
+	  for (let k=0; k<words.length; k++){
+	   letters.push(words[k].split(reg).filter(i => i))
+	  }
+	  
+	  for (let k=0; k < letters.length; k++){
+		let word = []
+		for(let i = 0; i < letters[k].length; i++){
+		word.push( letters[k][i].replace(reg1, ".").replace(reg2, "-").replace(reg3, ""))
+		 }
+	  decoded.push(word.map(a => MORSE_TABLE[a]).join(''))
+	  
+	  }
+	   
+	  return decoded.join(' ')
+	  
+	  }
 
-	for (let i = 0; i < expr.length; i++) {
-		if (expr[i] == " ") {
-			newArr.push("@");
-		}
-
-		for (const letter in MORSE_TABLE) {
-			if (MORSE_TABLE[letter] === expr[i]) {
-				newArr.push(letter);
-			}
-		}
-	}
-
-	for (let i = 0; i < newArr.length; i++) {
-		let morseLetter = "";
-		for (let k = 0; k < newArr[i].length; k++) {
-			if (newArr[i][k] === ".") {
-				morseLetter = morseLetter + '10';
-			} else if (newArr[i][k] === "-") {
-				morseLetter = morseLetter + '11';
-			} else if (newArr[i][k] === "@") {
-				morseLetter = morseLetter + "**********";
-			}
-		}
-
-		morseArr.push(morseLetter.padStart(10, '0'));
-	}
-
-	const result = morseArr.join('');
-    return result;
-}
 
 module.exports = {
-    decode
-}
+	decode
+};
